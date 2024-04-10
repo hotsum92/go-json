ed33880a0928d422b99c0ae3d5331d972bac5bd0
832022 <hotsuma_kawashima@densan-s.co.jp>
2024-04-10 09:39:29 +0900

    return json

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,8 +16,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	t := map[string]string{"hello": "world"}
+
 	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
