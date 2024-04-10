bb8722e1f0f7f65fdc8bb1ef0086e698f75f96a5
832022 <hotsuma_kawashima@densan-s.co.jp>
2024-04-10 10:41:07 +0900

    implement server

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"os"
 )
+
+var defaultJson = []byte(`{"key": "value"}`)
+var byPath = map[string]string{}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	dump, err := httputil.DumpRequest(r, true)
@@ -16,19 +21,94 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	t := map[string]string{"hello": "world"}
-
 	fmt.Println(string(dump))
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
 
-	if err := json.NewEncoder(w).Encode(t); err != nil {
+	path := byPath[r.URL.Path]
+
+	if path == "" {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(defaultJson)
+
+		return
+	}
+
+	file, err := os.Open(path)
+
+	if err != nil {
 		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
 		return
 	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+
+	bytes := make([]byte, stat.Size())
+	_, err = bufio.NewReader(file).Read(bytes)
+	if err != nil && err != io.EOF {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(bytes)
 }
 
 func main() {
+
+	if (len(os.Args) % 2) == 1 {
+		for i, v := range os.Args {
+			if i == 0 {
+				continue
+			}
+
+			if (i+1)%2 == 0 {
+				log.Printf("define %v -> %v", v, os.Args[i+1])
+				byPath[v] = os.Args[i+1]
+			}
+		}
+	} else {
+		for i, v := range os.Args {
+			if i == 0 {
+				continue
+			}
+
+			if i == 1 {
+				log.Printf("define default -> %v", os.Args[i])
+				file, err := os.Open(os.Args[i])
+
+				if err != nil {
+					log.Fatalf("failed to open default json file: %v", err)
+				}
+				defer file.Close()
+
+				stat, err := file.Stat()
+				if err != nil {
+					log.Fatalf("failed to get stat of default json file: %v", err)
+				}
+
+				bytes := make([]byte, stat.Size())
+				_, err = bufio.NewReader(file).Read(bytes)
+
+				if err != nil && err != io.EOF {
+					log.Fatalf("failed to read default json file: %v", err)
+				}
+
+				defaultJson = bytes
+			}
+
+			if (i+1)%2 == 1 {
+				log.Printf("define %v -> %v", v, os.Args[i+1])
+				byPath[v] = os.Args[i+1]
+			}
+		}
+	}
+
 	var httpServer http.Server
 	http.HandleFunc("/", handler)
 	log.Println("start http listening :18888")
